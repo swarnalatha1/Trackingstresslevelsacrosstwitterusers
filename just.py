ed8769cfa60464f982e38fe1619df41f1637a3e5
swarnalatha <ale.swarnalatha@gmail.com>
2020-01-25 21:48:03 +0000

    sort

--- just.py
+++ just.py
@@ -1,10 +1,14 @@
-r = int(input('Enter the number'))
-w = r%2
-if w == 0:
-    print('even')
-    if r>=10:
-        print('super')
-    else:
-        print('oops')
-elif w==1:
-    print('odd')  
+#r = int(input('Enter the number'))
+#w = r%2
+#if w == 0:
+    #print('even')
+    #if r>=10:
+   #  else:
+ #       print('oops')
+#elif w==1:
+ #   print('odd')
+
+
+x = [3,5,1,7]
+x.sort()
+print(x)
