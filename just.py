8f4bb35bbd4a9ff34b03d59fb03956d4331b82aa
swarnalatha <ale.swarnalatha@gmail.com>
2020-01-26 13:10:12 +0000

    code for a vending machine

--- just.py
+++ just.py
@@ -8,7 +8,61 @@
 #elif w==1:
  #   print('odd')
 
-
-x = [4,5,2,7,5,8]
+"""
+#x = [4,5,2,7,5,8]
 x.reverse()
 print(x)
+
+w = input("enter a string")
+i = 1
+while i <= 3:
+    print(w,end="")
+    i = i + 1
+
+    j = 1
+    while j <= 3:
+        print('speed', end = "")
+        j = j+1
+        k = 1
+        while k <= 3:
+            print("your awesome", end="")
+            k = k+1
+
+    print()
+    """
+"""""
+s ='swarna'
+for i in s:
+    print(i)
+"""""
+av =10
+r = int(input("Enter the no of candies"))
+i=1
+while i<=r:
+    if i >= av:
+        print('out of stock')
+        break
+    print('candy')
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
